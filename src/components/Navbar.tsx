a0ae0a457ecd19972494f2e156fc7c7a804f92f4
tawhidib <tawhidib@gmail.com>
2023-03-17 10:52:35 +0600

    feat: default theme changed to dark

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
-import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import List from "@mui/material/List";
@@ -19,7 +18,7 @@ interface NavbarProps {
   window?: () => Window;
 }
 
-const drawerWidth = 240;
+// const drawerWidth = 240;
 const navItems = [
   "Home",
   "About Me",
@@ -48,6 +47,7 @@ const Navbar: React.FC = (props: NavbarProps) => {
         alignItems: "center",
         justifyContent: "center",
         position: "relative",
+        // backgroundColor: "#0E1628",
       }}
     >
       <Box
@@ -60,14 +60,17 @@ const Navbar: React.FC = (props: NavbarProps) => {
         }}
       >
         <IconButton>
-          <CloseIcon sx={{ fontSize: "2rem" }} />
+          <CloseIcon sx={{ fontSize: "2rem", color: "#fff" }} />
         </IconButton>
       </Box>
       <List sx={{ width: "100%" }}>
         {navItems.map((item) => (
           <ListItem key={item}>
             <ListItemButton sx={{ textAlign: "center", display: "block" }}>
-              <ListItemText primary={item} sx={{ display: "block" }} />
+              <ListItemText
+                primary={item}
+                sx={{ display: "block", color: "whitesmoke" }}
+              />
             </ListItemButton>
           </ListItem>
         ))}
@@ -118,6 +121,12 @@ const Navbar: React.FC = (props: NavbarProps) => {
         <Drawer
           container={container}
           variant="temporary"
+          anchor="top"
+          transitionDuration={{
+            appear: 320,
+            enter: 320,
+            exit: 320,
+          }}
           open={mobileOpen}
           onClose={handleDrawerToggle}
           ModalProps={{
@@ -134,48 +143,53 @@ const Navbar: React.FC = (props: NavbarProps) => {
           {drawer}
         </Drawer>
       </Box>
-      <Box component="main" sx={{ p: 3 }}>
-        <Toolbar />
-        <Typography>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
-          unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
-          quibusdam, aliquam dolore excepturi quae. Distinctio enim at eligendi
-          perferendis in cum quibusdam sed quae, accusantium et aperiam? Quod
-          itaque exercitationem, at ab sequi qui modi delectus quia corrupti
-          alias distinctio nostrum. Minima ex dolor modi inventore sapiente
-          necessitatibus aliquam fuga et. Sed numquam quibusdam at officia
-          sapiente porro maxime corrupti perspiciatis asperiores, exercitationem
-          eius nostrum consequuntur iure aliquam itaque, assumenda et! Quibusdam
-          temporibus beatae doloremque voluptatum doloribus soluta accusamus
-          porro reprehenderit eos inventore facere, fugit, molestiae ab officiis
-          illo voluptates recusandae. Vel dolor nobis eius, ratione atque
-          soluta, aliquam fugit qui iste architecto perspiciatis. Nobis,
-          voluptatem! Cumque, eligendi unde aliquid minus quis sit debitis
-          obcaecati error, delectus quo eius exercitationem tempore. Delectus
-          sapiente, provident corporis dolorum quibusdam aut beatae repellendus
-          est labore quisquam praesentium repudiandae non vel laboriosam quo ab
-          perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
-          commodi nihil corrupti cum non fugiat praesentium doloremque
-          architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
-          molestiae dicta blanditiis est expedita eius debitis cupiditate porro
-          sed aspernatur quidem, repellat nihil quasi praesentium quia eos,
-          quibusdam provident. Incidunt tempore vel placeat voluptate iure
-          labore, repellendus beatae quia unde est aliquid dolor molestias
-          libero. Reiciendis similique exercitationem consequatur, nobis placeat
-          illo laudantium! Enim perferendis nulla soluta magni error, provident
-          repellat similique cupiditate ipsam, et tempore cumque quod! Qui, iure
-          suscipit tempora unde rerum autem saepe nisi vel cupiditate iusto.
-          Illum, corrupti? Fugiat quidem accusantium nulla. Aliquid inventore
-          commodi reprehenderit rerum reiciendis! Quidem alias repudiandae eaque
-          eveniet cumque nihil aliquam in expedita, impedit quas ipsum nesciunt
-          ipsa ullam consequuntur dignissimos numquam at nisi porro a, quaerat
-          rem repellendus. Voluptates perspiciatis, in pariatur impedit, nam
-          facilis libero dolorem dolores sunt inventore perferendis, aut
-          sapiente modi nesciunt.
-        </Typography>
-      </Box>
     </Box>
   );
 };
 
 export default Navbar;
+
+{
+  /* <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            unde fugit veniam eius, perspiciatis sunt? Corporis qui ducimus
+            quibusdam, aliquam dolore excepturi quae. Distinctio enim at
+            eligendi perferendis in cum quibusdam sed quae, accusantium et
+            aperiam? Quod itaque exercitationem, at ab sequi qui modi delectus
+            quia corrupti alias distinctio nostrum. Minima ex dolor modi
+            inventore sapiente necessitatibus aliquam fuga et. Sed numquam
+            quibusdam at officia sapiente porro maxime corrupti perspiciatis
+            asperiores, exercitationem eius nostrum consequuntur iure aliquam
+            itaque, assumenda et! Quibusdam temporibus beatae doloremque
+            voluptatum doloribus soluta accusamus porro reprehenderit eos
+            inventore facere, fugit, molestiae ab officiis illo voluptates
+            recusandae. Vel dolor nobis eius, ratione atque soluta, aliquam
+            fugit qui iste architecto perspiciatis. Nobis, voluptatem! Cumque,
+            eligendi unde aliquid minus quis sit debitis obcaecati error,
+            delectus quo eius exercitationem tempore. Delectus sapiente,
+            provident corporis dolorum quibusdam aut beatae repellendus est
+            labore quisquam praesentium repudiandae non vel laboriosam quo ab
+            perferendis velit ipsa deleniti modi! Ipsam, illo quod. Nesciunt
+            commodi nihil corrupti cum non fugiat praesentium doloremque
+            architecto laborum aliquid. Quae, maxime recusandae? Eveniet dolore
+            molestiae dicta blanditiis est expedita eius debitis cupiditate
+            porro sed aspernatur quidem, repellat nihil quasi praesentium quia
+            eos, quibusdam provident. Incidunt tempore vel placeat voluptate
+            iure labore, repellendus beatae quia unde est aliquid dolor
+            molestias libero. Reiciendis similique exercitationem consequatur,
+            nobis placeat illo laudantium! Enim perferendis nulla soluta magni
+            error, provident repellat similique cupiditate ipsam, et tempore
+            cumque quod! Qui, iure suscipit tempora unde rerum autem saepe nisi
+            vel cupiditate iusto. Illum, corrupti? Fugiat quidem accusantium
+            nulla. Aliquid inventore commodi reprehenderit rerum reiciendis!
+            Quidem alias repudiandae eaque eveniet cumque nihil aliquam in
+            expedita, impedit quas ipsum nesciunt ipsa ullam consequuntur
+            dignissimos numquam at nisi porro a, quaerat rem repellendus.
+            Voluptates perspiciatis, in pariatur impedit, nam facilis libero
+            dolorem dolores sunt inventore perferendis, aut sapiente modi
+            nesciunt.
+          </Typography>
+        </Box> */
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import CustomTheme from "@/lib/themeCumstomization";
+import "@/styles/globals.css";
+import ThemeProvider from "@mui/material/";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CustomTheme>
+      <Component {...pageProps} />
+    </CustomTheme>
+  );
 }
